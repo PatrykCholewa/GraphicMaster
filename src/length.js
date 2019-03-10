f7299c392918786a895778224508c9ba9a8c5aa0
Patryk Cholewa <patr.cholewa@gmail.com>
2019-03-10 18:19:49 +0100

    refactor

--- src/length.js
+++ src/length.js
@@ -8,7 +8,6 @@ export const getLengthMiddlePoint = length => length[0].plus(length[1])
 export const getLengthWidth = length => {
     const lengthEnd = asVect(length[1]);
     const squaredWidth = length[0].eleMap( (val, row, col) => (val - lengthEnd[col]) * (val - lengthEnd[col]))
-                                  .toArray()[0]
-                                  .reduce( (acc, val) => acc + val, 0);
+                                  .getSum();
     return Math.sqrt(squaredWidth);
 };
