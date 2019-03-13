dda31f0ecd240ec77b0bac9408c2f702e205243a
Patryk Cholewa <patr.cholewa@gmail.com>
2019-03-14 00:19:32 +0100

    add basic moves

--- src/objects.js
+++ src/objects.js
@@ -8,8 +8,8 @@ directions[Camera.DIRECTION.LEFT] = new Vector([1, 0, 0]);
 directions[Camera.DIRECTION.RIGHT] = new Vector([-1, 0, 0]);
 directions[Camera.DIRECTION.UP] = new Vector([0, 1, 0]);
 directions[Camera.DIRECTION.DOWN] = new Vector([0, -1, 0]);
-directions[Camera.DIRECTION.IN] = new Vector([0, 0, 1]);
-directions[Camera.DIRECTION.OUT] = new Vector([0, 0, -1]);
+// directions[Camera.DIRECTION.IN] = new Vector([0, 0, 1]);
+// directions[Camera.DIRECTION.OUT] = new Vector([0, 0, -1]);
 
 export const INITIAL_CAMERA = new Camera(new Plain([0, 0, 1, 0]), new Vector([0, 0, 5]), directions);
 
